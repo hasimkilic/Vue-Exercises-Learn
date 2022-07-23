20763f7bb418e3639e157006bd31daa34eda74a6
Haşim Kılıç <93584838+hasimkilic@users.noreply.github.com>
2022-07-23 22:28:05 +0300

    Add files via upload

---
+++ vue-provide-inject-slot/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- <ModalComp :title="'Modal Title'" :content="'Content'"/>  Prop ile kullandığımz durum bu şekildedir. Ama proje büydükçe karışıklık yaratır.-->
+    <ModalComp :onSave="onSave" >
+      <!-- <h3>Slot İle gelen Title Bilgisi</h3> --> <!--ModalComp tan gelen slot bilgisi bu şekilde yazdırılır.-->
+      <template #title><!--Bu şekilde name belirterek slottan çekiyoruz veriyi-->
+        <h3>Slot İle gelen Title Bilgisi</h3>
+      </template>
+      <!-- <template v-slot:content></template>  Aşağıdaki kullanım ile aynı şey-->
+      <template #content>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ullam ex quos ut tempora? Saepe suscipit sequi nulla adipisci quia ipsum alias nihil, sed dolorum iusto quo temporibus aperiam laborum repellat nesciunt culpa, inventore quis eveniet ea corporis distinctio quae, iste sint cupiditate! Placeat illum pariatur, minus in reiciendis mollitia.
+        </p>
+      </template>
+      <!--Aşağıdaki şekildeki gibi kullanımı vardır. slot props yöntemi -->
+      <template v-slot="slotProps">
+        {{slotProps.text}}
+        {{slotProps.count}}
+      </template>
+    </ModalComp>
+  </div>
+</template>
+
+<script>
+import ModalComp from '@/components/ModalComp'
+
+  export default {
+    components:{
+      ModalComp
+    },
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      onSave(){
+        alert('Saved')
+      }
+    },
+  }
+</script>
---
+++ vue-provide-inject-slot/src/components/ModalComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <slot name="title"/>
+        </div>
+        <hr>
+        <div class="content">
+            <slot name="content"/>
+        </div>
+        <hr>
+        <div class="footer">
+            <button>Kapat</button>
+            <button @click="onSave" >Kaydet</button>
+        </div>
+        <div>
+            <slot :text="'Bu değişik slot kullanımı'" :count="1" />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['onSave','content','title'],
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
